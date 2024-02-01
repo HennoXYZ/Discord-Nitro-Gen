f75e1b8383b22118be419ede8722feb372ce39c8
Henno <96645357+HennoXYZ@users.noreply.github.com>
2024-02-01 14:49:19 +0300

    Update app.py

--- Discord Nitro Generator/app.py
+++ Discord Nitro Generator/app.py
@@ -2,19 +2,16 @@ import random
 import string
 import requests as re
 
-f = open("trying.txt","r+")
 char = string.ascii_letters + string.digits + "@"
 rnd = ""
+splt = []
 
 while True:
     for l in range(17):
         rnd += random.choice(char)
     link = "https://discord.gift/"+rnd
-    read = f.read()
-    splt = read.split(",\n")
-    splt.pop(-1)
     if link not in splt:
-        f.write("https://discord.gift/"+rnd+",\n")
+        splt.append("https://discord.gift/"+rnd)
         res = re.get(link)
         html = res.text
         if "redeem" in html.lower():
